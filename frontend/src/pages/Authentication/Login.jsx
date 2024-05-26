6c2eda5e850411e28b5963dba90a2bff32e16934
Abhijeet Kumar <81794591+Abhi-wolf@users.noreply.github.com>
2024-05-26 12:12:56 +0530

    Delete frontend directory

--- frontend/src/pages/Authentication/Login.jsx
+++
@@ -1,9 +0,0 @@
-import { LoginForm } from "@/components/LoginForm";
-
-export default function Login() {
-  return (
-    <div className="w-full min-h-[90vh] flex justify-center items-center">
-      <LoginForm />
-    </div>
-  );
-}
--- frontend/src/pages/Authentication/SignUp.jsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "../../components/SignUpForm";
-
-export function SignUp() {
-  return (
-    <div className="w-full min-h-[90vh] flex justify-center items-center ">
-      <SignUpForm />
-    </div>
-  );
-}
